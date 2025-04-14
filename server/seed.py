592c3787ca4dd1218054ad276ef17755907757cd
Prince-gk <prince.georanja@gmail.com>
2025-04-14 13:23:30 +0300

    Refactor code style for consistency and readability; add user authentication endpoints and update database migration scripts.

--- server/seed.py
+++ server/seed.py
@@ -10,7 +10,6 @@ from models import db, Article, User
 fake = Faker()
 
 with app.app_context():
-
     print("Deleting all records...")
     Article.query.delete()
     User.query.delete()
@@ -21,11 +20,10 @@ with app.app_context():
     users = []
     usernames = []
     for i in range(25):
-
         username = fake.first_name()
         while username in usernames:
             username = fake.first_name()
-        
+
         usernames.append(username)
 
         user = User(username=username)
@@ -37,19 +35,19 @@ with app.app_context():
     articles = []
     for i in range(100):
         content = fake.paragraph(nb_sentences=8)
-        preview = content[:25] + '...'
-        
+        preview = content[:25] + "..."
+
         article = Article(
             author=fake.name(),
             title=fake.sentence(),
             content=content,
             preview=preview,
-            minutes_to_read=randint(1,20),
+            minutes_to_read=randint(1, 20),
         )
 
         articles.append(article)
 
     db.session.add_all(articles)
-    
+
     db.session.commit()
     print("Complete.")
